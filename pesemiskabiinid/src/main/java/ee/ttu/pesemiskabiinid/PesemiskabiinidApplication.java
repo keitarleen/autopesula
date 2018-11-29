d2bb4663a987bc4cb084026db5d35964cb371b15
keitarleen <keitarleenkalle@gmail.com>
2018-11-29 11:34:32 +0200

    Added Spring and React files

---
+++ pesemiskabiinid/src/main/java/ee/ttu/pesemiskabiinid/PesemiskabiinidApplication.java
@@ -0,0 +1,12 @@
+package ee.ttu.pesemiskabiinid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesemiskabiinidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesemiskabiinidApplication.class, args);
+	}
+}
