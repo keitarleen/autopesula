13eb16b79600553468e86d75a6d1357349210566
Kaspar Kivistik <kkivis@ttu.ee>
2018-11-29 12:17:12 +0200

    Database connection created

---
+++ pesemiskabiinid/src/main/java/ee/ttu/pesemiskabiinid/config/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package ee.ttu.pesemiskabiinid.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class DatabaseConfig {
+
+    @Value("${db.host}")
+    String url;
+
+    @Value("${db.username}")
+    String username;
+
+    @Value("${db.password}")
+    String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
+    }
+}
