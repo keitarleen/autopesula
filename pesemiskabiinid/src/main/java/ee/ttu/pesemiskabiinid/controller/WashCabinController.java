2d82b03dda3b66a3e154ae28cdc43d1cbcd489c6
Kaspar Kivistik <kkivis@ttu.ee>
2018-11-30 11:01:53 +0200

    CORS

--- pesemiskabiinid/src/main/java/ee/ttu/pesemiskabiinid/controller/WashCabinController.java
+++ pesemiskabiinid/src/main/java/ee/ttu/pesemiskabiinid/controller/WashCabinController.java
@@ -4,16 +4,14 @@ import ee.ttu.pesemiskabiinid.model.WashCabin;
 import ee.ttu.pesemiskabiinid.model.WashCabinDto;
 import ee.ttu.pesemiskabiinid.service.WashCabinService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/cabin")
+@CrossOrigin
 public class WashCabinController {
 
     private WashCabinService service;
